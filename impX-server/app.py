67464cb632a4930e18e5376b00e6bacd5ea56056
c-rangavamsi <84913444+c-rangavamsi@users.noreply.github.com>
2022-02-07 22:40:18 +0530

    queries refactor

--- impX-server/app.py
+++ impX-server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
+import json
 
 app = Flask(__name__)
 
@@ -19,9 +20,16 @@ def hello_world():
 def get_all_screens():
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute(''' SELECT ext_screen_id, name, publisher_id, venue_type, geo_location, status, activation_date FROM screens WHERE status != 2''')
+        cursor.execute(''' SELECT ext_screen_id as screenId, name as name, publisher_id as publisherId, venue_type as venueType, geo_location as geoLocation, status as status FROM screens WHERE status != 2''')
+        row_headers = [x[0] for x in cursor.description]
         rows = cursor.fetchall()
-        resp = jsonify(rows)
+        result = []
+        for row in rows:
+            result.append(dict(zip(row_headers, row)))
+        for res in result:
+            res['geoLocation'] = json.loads(res['geoLocation'])
+            res['publisherName'] = ""
+        resp = jsonify(result)
         resp.status_code = 200
         return resp
     except Exception as e:
@@ -33,9 +41,39 @@ def get_all_screens():
 def get_screen(screenId):
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM screens WHERE ext_screen_id = '{0}' and status != 2".format(screenId))
-        rows = cursor.fetchone()
-        resp = jsonify(rows)
+        cursor.execute('''SELECT ext_screen_id as screenId, name, venue_id as venueId, 
+        publisher_id as publisherId, timezone, utc_offset as utcOffset, cms_name as cmsName, geo_location as geoLocation, address, 
+        position, latitude, longitude, venue_type as venueType, direction, manufacturer, side, dimension_width as dimensionWidth,
+        dimension_height as dimensionHeight, resolution_width as resolutionWidth, resolution_height as resolutionHeight, orientation, 
+        audience_data_provider as audienceDataProvider, breakdown_unit as breakdownUnit, impression_multiplier as impressionMultiplier,
+        default_ad_duration as defaultAdDuration, impression_multiplier_per_second as impressionMultiplierPerSecond,
+        direct_deals as directDeals, exchange_deals as exchangeDeals, static_support as staticSupport, video_support as videoSupport, 
+        blocked_cat, blocked_adv, blocked_demand_channel, currency, cpm, status 
+        FROM screens WHERE ext_screen_id = '{0}' and status != 2'''.format(screenId))
+        row_headers = [x[0] for x in cursor.description]
+        row = cursor.fetchone()
+        result = []
+        result.append(dict(zip(row_headers, row)))
+        res = result[0]
+        res['address'] = json.loads(res['address'])
+        res['geoLocation'] = json.loads(res['geoLocation'])
+        res['brandSafetyConfig'] = {
+            'blockedCategories': res['blocked_cat'],
+            'blockedAdvertisersDomains': res['blocked_adv'],
+            'blockedDemandChannels': res['blocked_demand_channel']
+        }
+        res.pop("blocked_cat")
+        res.pop('blocked_adv')
+        res.pop('blocked_demand_channel')
+        res['floorPrice'] = {
+            'cpm': res['cpm'],
+            'currency': res['currency']
+        }
+        res.pop('cpm')
+        res.pop('currency')
+        res['adUnits'] = []
+       
+        resp = jsonify(res)
         resp.status_code = 200
         return resp
     except Exception as e:
